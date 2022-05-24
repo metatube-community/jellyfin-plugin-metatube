eeeef3a361deea8b11065c5b1585e386e2fe39cf
xjasonlyu <xjasonlyu@gmail.com>
2022-05-24 22:43:16 +0800

    Add const

--- Jellyfin.Plugin.JavTube/Constant.cs
+++ Jellyfin.Plugin.JavTube/Constant.cs
@@ -2,11 +2,15 @@ namespace Jellyfin.Plugin.JavTube;
 
 public static class Constant
 {
-    public static readonly string JavTube = "JavTube";
+    public const string JavTube = "JavTube";
 
-    public static readonly string Description = "Just Another Video Tube";
+    public const string JavTubeId = "JavTubeId";
 
-    public static readonly string Guid = "df87283d-7224-4f9c-a448-3433d9cf278a";
+    public const string JavTubeProvider = "JavTubeProvider";
+
+    public const string Description = "Just Another Video Tube";
+
+    public const string Guid = "df87283d-7224-4f9c-a448-3433d9cf278a";
 
     public static readonly string UserAgent = $"Jellyfin.Plugin.JavTube/{Plugin.Instance.Version}";
 }
