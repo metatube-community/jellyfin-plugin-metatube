b8cbc90b52330526775dabe6ca26f6beaa8a28e8
xjasonlyu <xjasonlyu@gmail.com>
2022-05-26 21:21:57 +0800

    remove constant

--- Jellyfin.Plugin.JavTube/Constant.cs
+++ Jellyfin.Plugin.JavTube/Constant.cs
@@ -4,10 +4,6 @@ public static class Constant
 {
     public const string JavTube = "JavTube";
 
-    public const string JavTubeId = "JavTubeId";
-
-    public const string JavTubeProvider = "JavTubeProvider";
-
     public const string Description = "Just Another Video Tube";
 
     public const string Guid = "df87283d-7224-4f9c-a448-3433d9cf278a";
