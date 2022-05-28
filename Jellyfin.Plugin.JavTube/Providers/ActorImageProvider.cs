abbc059a124ecc976daa6b31c711d4d621ec1c50
xjasonlyu <xjasonlyu@gmail.com>
2022-05-28 22:56:29 +0800

    Add ActorProvider

---
+++ Jellyfin.Plugin.JavTube/Providers/ActorImageProvider.cs
@@ -0,0 +1,66 @@
+using Jellyfin.Plugin.JavTube.Extensions;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Providers;
+using MediaBrowser.Model.Entities;
+using MediaBrowser.Model.Providers;
+#if __EMBY__
+using MediaBrowser.Common.Net;
+using MediaBrowser.Model.Logging;
+using MediaBrowser.Model.Configuration;
+#else
+using Microsoft.Extensions.Logging;
+#endif
+
+namespace Jellyfin.Plugin.JavTube.Providers;
+
+public class ActorImageProvider : BaseProvider, IRemoteImageProvider, IHasOrder
+{
+#if __EMBY__
+    public ActorImageProvider(IHttpClient httpClient, ILogManager logManager) :
+        base(httpClient, logManager.CreateLogger<ActorImageProvider>())
+#else
+        public ActorImageProvider(IHttpClientFactory httpClientFactory, ILogger<ActorImageProvider> logger) : base(
+            httpClientFactory, logger)
+#endif
+    {
+        // Nothing
+    }
+
+    public int Order => 1;
+
+    public string Name => Constant.JavTube;
+
+#if __EMBY__
+    public async Task<IEnumerable<RemoteImageInfo>> GetImages(BaseItem item, LibraryOptions libraryOptions,
+        CancellationToken cancellationToken)
+#else
+        public async Task<IEnumerable<RemoteImageInfo>> GetImages(BaseItem item, CancellationToken cancellationToken)
+#endif
+    {
+        var pm = item.GetProviderModel(Name);
+        if (string.IsNullOrWhiteSpace(pm.Id) || string.IsNullOrWhiteSpace(pm.Provider))
+            return new List<RemoteImageInfo>();
+
+        var actorInfo = await ApiClient.GetActorInfo(pm.Id, pm.Provider, cancellationToken);
+
+        return actorInfo.Images.Select(image => new RemoteImageInfo
+        {
+            ProviderName = Name, 
+            Type = ImageType.Primary,
+            Url = ApiClient.GetPrimaryImageApiUrl(actorInfo.Id, actorInfo.Provider, image)
+        }).ToList();
+    }
+
+    public bool Supports(BaseItem item)
+    {
+        return item is Person;
+    }
+
+    public IEnumerable<ImageType> GetSupportedImages(BaseItem item)
+    {
+        return new List<ImageType>
+        {
+            ImageType.Primary
+        };
+    }
+}
---
+++ Jellyfin.Plugin.JavTube/Providers/ActorProvider.cs
@@ -0,0 +1,110 @@
+using Jellyfin.Plugin.JavTube.Extensions;
+using Jellyfin.Plugin.JavTube.Models;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Providers;
+using MediaBrowser.Model.Providers;
+#if __EMBY__
+using MediaBrowser.Common.Net;
+using MediaBrowser.Model.Logging;
+#else
+using Microsoft.Extensions.Logging;
+#endif
+
+namespace Jellyfin.Plugin.JavTube.Providers;
+
+public class ActorProvider : BaseProvider, IRemoteMetadataProvider<Person, PersonLookupInfo>, IHasOrder
+{
+#if __EMBY__
+    public ActorProvider(IHttpClient httpClient, ILogManager logManager) : base(
+        httpClient,
+        logManager.CreateLogger<ActorProvider>())
+#else
+        public ActorProvider(IHttpClientFactory httpClientFactory, ILogger<ActorProvider> logger) : base(
+            httpClientFactory, logger)
+#endif
+    {
+        // Nothing
+    }
+
+    public int Order => 1;
+
+    public string Name => Constant.JavTube;
+
+    public async Task<MetadataResult<Person>> GetMetadata(PersonLookupInfo info,
+        CancellationToken cancellationToken)
+    {
+        var pm = info.GetProviderModel(Name);
+        if (string.IsNullOrWhiteSpace(pm.Id) || string.IsNullOrWhiteSpace(pm.Provider))
+        {
+            var searchResults = (await GetSearchResults(info, cancellationToken)
+                .ConfigureAwait(false)).ToList();
+            if (searchResults.Any())
+            {
+                var firstResult = searchResults.First();
+                pm = firstResult.GetProviderModel(Name);
+            }
+        }
+
+        var m = await ApiClient.GetActorInfo(pm.Id, pm.Provider, cancellationToken);
+
+        var result = new MetadataResult<Person>
+        {
+            Item = new Person
+            {
+                Name = m.Name,
+                PremiereDate = m.Birthday.ValidDateTime(),
+                ProductionYear = m.Birthday.ValidDateTime()?.Year,
+                Overview = FormatOverview(m)
+            },
+            HasMetadata = true
+        };
+
+        if (!string.IsNullOrWhiteSpace(m.Nationality))
+            result.Item.ProductionLocations = new[] { m.Nationality };
+
+        result.Item.SetProviderModel(Name, m);
+
+        return result;
+    }
+
+    public async Task<IEnumerable<RemoteSearchResult>> GetSearchResults(
+        PersonLookupInfo info, CancellationToken cancellationToken)
+    {
+        var pm = info.GetProviderModel(Name);
+        if (string.IsNullOrWhiteSpace(pm.Id))
+            pm.Id = info.Name;
+
+        var searchResults = await ApiClient.SearchActor(pm.Id, pm.Provider, cancellationToken);
+
+        var results = new List<RemoteSearchResult>();
+        foreach (var m in searchResults)
+        {
+            var result = new RemoteSearchResult
+            {
+                Name = m.Name,
+                SearchProviderName = Name
+            };
+            if (m.Images.Length > 0)
+                result.ImageUrl = ApiClient.GetPrimaryImageApiUrl(m.Id, m.Provider, m.Images[0], auto: true);
+            result.SetProviderModel(Name, m);
+            results.Add(result);
+        }
+
+        return results;
+    }
+
+    private static string FormatOverview(ActorInfoModel a)
+    {
+        string G(string k, string v)
+        {
+            return !string.IsNullOrWhiteSpace(v) ? $"{k}: {v}\n" : string.Empty;
+        }
+
+        var overview = string.Empty;
+        overview += G("身高", $"{a.Height}cm");
+        overview += G("血型", $"{a.BloodType}");
+        overview += G("罩杯", $"{a.CupSize}");
+        overview += G("三围", $"{a.Measurements}");
+        return overview;
+    }
+}
