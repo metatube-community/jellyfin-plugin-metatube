efdae62556a910d048086dc7c978611a168c0ab1
xjasonlyu <xjasonlyu@gmail.com>
2022-05-28 15:18:02 +0800

    update constant

--- Jellyfin.Plugin.JavTube/Constant.cs
+++ Jellyfin.Plugin.JavTube/Constant.cs
@@ -4,11 +4,9 @@ public static class Constant
 {
     public const string JavTube = "JavTube";
 
-    public const string GFriends = "GFriends";
-
     public const string Description = "Just Another Video Tube";
 
     public const string Guid = "df87283d-7224-4f9c-a448-3433d9cf278a";
 
-    public static readonly string UserAgent = $"{Plugin.Instance.Namespace}/{Plugin.Instance.Version}";
+    public static readonly string UserAgent = $"{typeof(Plugin).Namespace}/{Plugin.Instance.Version}";
 }
