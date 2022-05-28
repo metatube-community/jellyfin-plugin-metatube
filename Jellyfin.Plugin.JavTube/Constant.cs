e0aed25c06b4e46c5e11b9be81b1590d6801a16d
xjasonlyu <xjasonlyu@gmail.com>
2022-05-28 15:22:02 +0800

    Add rating

--- Jellyfin.Plugin.JavTube/Constant.cs
+++ Jellyfin.Plugin.JavTube/Constant.cs
@@ -8,5 +8,7 @@ public static class Constant
 
     public const string Guid = "df87283d-7224-4f9c-a448-3433d9cf278a";
 
+    public const string Rating = "JP-18+";
+
     public static readonly string UserAgent = $"{typeof(Plugin).Namespace}/{Plugin.Instance.Version}";
 }
