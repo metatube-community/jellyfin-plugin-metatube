72e6f566e2334ed58498980e573e09bedb2764ad
xjasonlyu <xjasonlyu@gmail.com>
2022-05-28 21:33:07 +0800

    Add ActorExternalId

---
+++ Jellyfin.Plugin.JavTube/ExternalIds/ActorExternalId.cs
@@ -0,0 +1,27 @@
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Providers;
+using MediaBrowser.Model.Entities;
+#if !__EMBY__
+using MediaBrowser.Model.Providers;
+#endif
+
+namespace Jellyfin.Plugin.JavTube.ExternalIds;
+
+public class ActorExternalId : IExternalId
+{
+#if __EMBY__
+    public string Name => Constant.JavTube;
+#else
+    public string ProviderName => Constant.JavTube;
+
+    public ExternalIdMediaType? Type => ExternalIdMediaType.Person;
+#endif
+    public string Key => Constant.JavTube;
+
+    public string UrlFormatString => "{0}";
+
+    public bool Supports(IHasProviderIds item)
+    {
+        return item is Person;
+    }
+}
