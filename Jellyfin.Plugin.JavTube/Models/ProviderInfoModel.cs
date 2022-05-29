8aaa944b7df8d9df983feeaf7b035bd57d0cf47c
xjasonlyu <xjasonlyu@gmail.com>
2022-05-29 15:18:42 +0800

    Add ProviderIdModel

--- Jellyfin.Plugin.JavTube/Models/ProviderInfoModel.cs
+++ Jellyfin.Plugin.JavTube/Models/ProviderInfoModel.cs
@@ -2,11 +2,14 @@ using System.Text.Json.Serialization;
 
 namespace Jellyfin.Plugin.JavTube.Models;
 
-public class ProviderInfoModel
+public class ProviderIdModel
 {
-    [JsonPropertyName("homepage")] public string Homepage { get; set; }
-
     [JsonPropertyName("id")] public string Id { get; set; }
 
     [JsonPropertyName("provider")] public string Provider { get; set; }
+}
+
+public class ProviderInfoModel : ProviderIdModel
+{
+    [JsonPropertyName("homepage")] public string Homepage { get; set; }
 }
