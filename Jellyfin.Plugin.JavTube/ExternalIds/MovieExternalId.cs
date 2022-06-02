fc82410c765cd63942fc0227674bf4484bbe7fb1
xjasonlyu <xjasonlyu@gmail.com>
2022-06-02 17:39:14 +0800

    use BaseExternalId

--- Jellyfin.Plugin.JavTube/ExternalIds/MovieExternalId.cs
+++ Jellyfin.Plugin.JavTube/ExternalIds/MovieExternalId.cs
@@ -7,20 +7,13 @@ using MediaBrowser.Model.Providers;
 
 namespace Jellyfin.Plugin.JavTube.ExternalIds;
 
-public class MovieExternalId : IExternalId
+public class MovieExternalId : BaseExternalId, IExternalId
 {
-#if __EMBY__
-    public string Name => Plugin.Instance.Name;
-#else
-    public string ProviderName => Plugin.Instance.Name;
-
-    public ExternalIdMediaType? Type => ExternalIdMediaType.Movie;
+#if !__EMBY__
+    public override ExternalIdMediaType? Type => ExternalIdMediaType.Movie;
 #endif
-    public string Key => Plugin.Instance.Name;
 
-    public string UrlFormatString => Plugin.Instance.Configuration.Server + "?redirect={0}";
-
-    public bool Supports(IHasProviderIds item)
+    public override bool Supports(IHasProviderIds item)
     {
         return item is Movie;
     }
