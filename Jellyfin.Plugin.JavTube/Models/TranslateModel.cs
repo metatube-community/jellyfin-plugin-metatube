9cb9714c913209b357a9221190da65ab1565b9f0
xjasonlyu <xjasonlyu@gmail.com>
2022-06-07 00:11:24 +0800

    Add TranslateModel

---
+++ Jellyfin.Plugin.JavTube/Models/TranslateModel.cs
@@ -0,0 +1,12 @@
+using System.Text.Json.Serialization;
+
+namespace Jellyfin.Plugin.JavTube.Models;
+
+public class TranslateModel
+{
+    [JsonPropertyName("from")] public string From { get; set; }
+
+    [JsonPropertyName("to")] public string To { get; set; }
+
+    [JsonPropertyName("translated_text")] public string TranslatedText { get; set; }
+}
