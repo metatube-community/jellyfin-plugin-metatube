3f535dc2ef28e2ec65da31761d119fa915d02b15
xjasonlyu <xjasonlyu@gmail.com>
2022-06-17 15:14:17 +0800

    Add tag

--- Jellyfin.Plugin.JavTube/Providers/MovieProvider.cs
+++ Jellyfin.Plugin.JavTube/Providers/MovieProvider.cs
@@ -85,6 +85,10 @@ public class MovieProvider : BaseProvider, IRemoteMetadataProvider<Movie, MovieI
         if (m.Tags?.Length > 0)
             result.Item.SetGenres(m.Tags);
 
+        // Add Tag.
+        if (!string.IsNullOrWhiteSpace(m.Label))
+            result.Item.AddTag(m.Label);
+
         // Add Studio.
         if (!string.IsNullOrWhiteSpace(m.Maker))
             result.Item.AddStudio(m.Maker);
