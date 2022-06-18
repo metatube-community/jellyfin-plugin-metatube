f3a0c6a14283f00f14b0cb1b5bcccef87795a6ed
xjasonlyu <xjasonlyu@gmail.com>
2022-06-18 22:21:57 +0800

    remove JavTrailerId

--- Jellyfin.Plugin.JavTube/Constant.cs
+++ Jellyfin.Plugin.JavTube/Constant.cs
@@ -2,7 +2,5 @@ namespace Jellyfin.Plugin.JavTube;
 
 public static class Constant
 {
-    public const string JavTrailerId = "JavTrailer";
-
     public static readonly string UserAgent = $"{Plugin.Instance.Name}/{Plugin.Instance.Version}";
 }
