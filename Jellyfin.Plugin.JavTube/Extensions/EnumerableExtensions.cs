b65ac8fe6d8b0fabcc92d3afa763a4413437bde9
xjasonlyu <xjasonlyu@gmail.com>
2022-06-29 15:25:26 +0800

    Add Substitute extension method

--- Jellyfin.Plugin.JavTube/Extensions/EnumerableExtensions.cs
+++ Jellyfin.Plugin.JavTube/Extensions/EnumerableExtensions.cs
@@ -6,4 +6,20 @@ internal static class EnumerableExtensions
     {
         return source.Select((item, index) => (index, item));
     }
+
+    public static IEnumerable<string> Substitute(this IEnumerable<string> source, Dictionary<string, string> table)
+    {
+        var target = new List<string>();
+
+        foreach (var item in source ?? Enumerable.Empty<string>())
+        {
+            if (!table.TryGetValue(item, out var value))
+                target.Add(item); // Add original item.
+            else if (!string.IsNullOrWhiteSpace(value))
+                target.Add(value); // Add replaced item.
+            // Ignore deleted item.
+        }
+
+        return target;
+    }
 }
