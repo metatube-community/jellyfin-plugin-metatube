cf018426eedc0ac1e460ee1168b9cc94a7261a5a
xjasonlyu <xjasonlyu@gmail.com>
2022-06-30 14:42:48 +0800

    cleanup code

--- Jellyfin.Plugin.JavTube/Extensions/ProviderIdExtensions.cs
+++ Jellyfin.Plugin.JavTube/Extensions/ProviderIdExtensions.cs
@@ -6,9 +6,7 @@ internal static class ProviderIdExtensions
 {
     public static ProviderId GetPid(this IHasProviderIds instance, string name)
     {
-        return !instance.ProviderIds.Any()
-            ? new ProviderId()
-            : ProviderId.Deserialize(instance.GetProviderId(name));
+        return ProviderId.Deserialize(instance.GetProviderId(name));
     }
 
     public static void SetPid(this IHasProviderIds instance, string name, string provider, string id,
