b61bea7efcb2a46485b064cbebc1aae98e737214
xjasonlyu <xjasonlyu@gmail.com>
2022-07-02 23:31:21 +0800

    Add HttpResponseMessage Extensions

--- Jellyfin.Plugin.JavTube/Extensions/EmbyExtensions.cs
+++ Jellyfin.Plugin.JavTube/Extensions/EmbyExtensions.cs
@@ -1,15 +1,35 @@
 #if __EMBY__
+using MediaBrowser.Common.Net;
 using MediaBrowser.Model.Logging;
 
 namespace Jellyfin.Plugin.JavTube.Extensions;
 
 public static class EmbyExtensions
 {
-    #region LogManagerExtensions
+    #region LogManager
 
     public static ILogger CreateLogger<T>(this ILogManager logManager)
     {
         return logManager.GetLogger($"{Plugin.Instance.Name}.{typeof(T).Name}");
+    }
+
+    #endregion
+
+    #region HttpResponseMessage
+
+    public static async Task<HttpResponseInfo> ToHttpResponseInfo(this HttpResponseMessage response)
+    {
+        if (response == null)
+            throw new ArgumentNullException(nameof(response));
+
+        return new HttpResponseInfo
+        {
+            Content = await response.Content.ReadAsStreamAsync(),
+            ContentLength = response.Content.Headers.ContentLength,
+            ContentType = response.Content.Headers.ContentType?.ToString(),
+            StatusCode = response.StatusCode,
+            Headers = response.Content.Headers.ToDictionary(kvp => kvp.Key, kvp => string.Join(", ", kvp.Value))
+        };
     }
 
     #endregion
