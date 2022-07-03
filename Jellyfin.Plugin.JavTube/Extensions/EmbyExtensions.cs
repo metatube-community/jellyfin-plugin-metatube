6aa2a087816916d0abeb7778a731410b33f0c775
xjasonlyu <xjasonlyu@gmail.com>
2022-07-03 12:08:30 +0800

    Remove HttpResponseMessage extension

--- Jellyfin.Plugin.JavTube/Extensions/EmbyExtensions.cs
+++ Jellyfin.Plugin.JavTube/Extensions/EmbyExtensions.cs
@@ -1,5 +1,4 @@
 #if __EMBY__
-using MediaBrowser.Common.Net;
 using MediaBrowser.Model.Logging;
 
 namespace Jellyfin.Plugin.JavTube.Extensions;
@@ -14,25 +13,5 @@ public static class EmbyExtensions
     }
 
     #endregion
-
-    #region HttpResponseMessage
-
-    public static async Task<HttpResponseInfo> ToHttpResponseInfo(this HttpResponseMessage response)
-    {
-        if (response == null)
-            throw new ArgumentNullException(nameof(response));
-
-        return new HttpResponseInfo
-        {
-            Content = await response.Content.ReadAsStreamAsync(),
-            ContentLength = response.Content.Headers.ContentLength,
-            ContentType = response.Content.Headers.ContentType?.ToString(),
-            StatusCode = response.StatusCode,
-            Headers = response.Content.Headers.ToDictionary(kvp => kvp.Key, kvp => string.Join(", ", kvp.Value))
-        };
-    }
-
-    #endregion
 }
-
 #endif
