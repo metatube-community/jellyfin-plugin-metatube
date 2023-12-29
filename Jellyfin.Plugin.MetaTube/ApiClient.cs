5f1ad1252487890467d2ae08999196e52a59ef99
xjasonlyu <xjasonlyu@gmail.com>
2023-12-29 10:52:06 -0500

    Feature(Api): add lang option

--- Jellyfin.Plugin.MetaTube/ApiClient.cs
+++ Jellyfin.Plugin.MetaTube/ApiClient.cs
@@ -56,12 +56,13 @@ public static class ApiClient
         });
     }
 
-    private static string ComposeSearchApiUrl(string path, string q, string provider, bool fallback)
+    private static string ComposeSearchApiUrl(string path, string q, string provider, string lang, bool fallback)
     {
         return ComposeUrl(path, new NameValueCollection
         {
             { "q", q },
             { "provider", provider },
+            { "lang", lang },
             { "fallback", fallback.ToString() }
         });
     }
@@ -178,7 +179,7 @@ public static class ApiClient
     public static async Task<List<ActorSearchResult>> SearchActorAsync(string q, string provider,
         bool fallback, CancellationToken cancellationToken)
     {
-        var apiUrl = ComposeSearchApiUrl(ActorSearchApi, q, provider, fallback);
+        var apiUrl = ComposeSearchApiUrl(ActorSearchApi, q, provider, string.Empty, fallback);
         return await GetDataAsync<List<ActorSearchResult>>(apiUrl, true, cancellationToken);
     }
 
@@ -191,13 +192,19 @@ public static class ApiClient
     public static async Task<List<MovieSearchResult>> SearchMovieAsync(string q, string provider,
         CancellationToken cancellationToken)
     {
-        return await SearchMovieAsync(q, provider, true /* default */, cancellationToken);
+        return await SearchMovieAsync(q, provider, string.Empty, true /* default */, cancellationToken);
     }
 
-    public static async Task<List<MovieSearchResult>> SearchMovieAsync(string q, string provider,
+    public static async Task<List<MovieSearchResult>> SearchMovieAsync(string q, string provider, string lang,
+        CancellationToken cancellationToken)
+    {
+        return await SearchMovieAsync(q, provider, lang, true /* default */, cancellationToken);
+    }
+
+    public static async Task<List<MovieSearchResult>> SearchMovieAsync(string q, string provider, string lang,
         bool fallback, CancellationToken cancellationToken)
     {
-        var apiUrl = ComposeSearchApiUrl(MovieSearchApi, q, provider, fallback);
+        var apiUrl = ComposeSearchApiUrl(MovieSearchApi, q, provider, lang, fallback);
         return await GetDataAsync<List<MovieSearchResult>>(apiUrl, true, cancellationToken);
     }
 
