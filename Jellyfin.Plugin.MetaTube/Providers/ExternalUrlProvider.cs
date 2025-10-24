99bc75a0c75410940961c1a8af8861c26f6ccc93
xjasonlyu <xjasonlyu@gmail.com>
2025-10-24 18:06:15 -0400

    Fix: display provider URLs in Jellyfin

---
+++ Jellyfin.Plugin.MetaTube/Providers/ExternalUrlProvider.cs
@@ -0,0 +1,29 @@
+#if !__EMBY__
+using Jellyfin.Plugin.MetaTube.ExternalIds;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Entities.Movies;
+using MediaBrowser.Controller.Providers;
+using MediaBrowser.Model.Entities;
+
+namespace Jellyfin.Plugin.MetaTube.Providers;
+
+public class ExternalUrlProvider : IExternalUrlProvider
+{
+    public string Name => Plugin.Instance.Name;
+
+    public IEnumerable<string> GetExternalUrls(BaseItem item)
+    {
+        var pid = item.GetProviderId(Name);
+
+        switch (item)
+        {
+            case Movie:
+                yield return string.Format((new MovieExternalId()).UrlFormatString, pid);
+                break;
+            case Person:
+                yield return string.Format((new ActorExternalId()).UrlFormatString, pid);
+                break;
+        }
+    }
+}
+#endif
