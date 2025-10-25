0ca3a7cfd2c841cf263bda21f71d210b1d2cd135
Topbcy <dissyu923@gmail.com>
2025-10-25 17:03:03 +0800

    Update ExternalUrlProvider.cs

--- Jellyfin.Plugin.MetaTube/Providers/ExternalUrlProvider.cs
+++ Jellyfin.Plugin.MetaTube/Providers/ExternalUrlProvider.cs
@@ -1,28 +1,32 @@
 #if !__EMBY__
-using Jellyfin.Plugin.MetaTube.ExternalIds;
+using Jellyfin.Plugin.MyTube.ExternalIds;
 using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Entities.Movies;
 using MediaBrowser.Controller.Providers;
 using MediaBrowser.Model.Entities;
 
-namespace Jellyfin.Plugin.MetaTube.Providers;
-
-public class ExternalUrlProvider : IExternalUrlProvider
+namespace Jellyfin.Plugin.MyTube.Providers
 {
-    public string Name => Plugin.Instance.Name;
-
-    public IEnumerable<string> GetExternalUrls(BaseItem item)
+    public class ExternalUrlProvider : IExternalUrlProvider
     {
-        var pid = item.GetProviderId(Name);
+        public string Name => Plugin.Instance.Name;
 
-        switch (item)
+        public IEnumerable<string> GetExternalUrls(BaseItem item)
         {
-            case Movie:
-                yield return string.Format((new MovieExternalId()).UrlFormatString, pid);
-                break;
-            case Person:
-                yield return string.Format((new ActorExternalId()).UrlFormatString, pid);
-                break;
+            if (item.TryGetProviderId(Plugin.Instance.Name, out var pid))
+            {
+                switch (item)
+                {
+                    case Movie:
+                        yield return string.Format(new MovieExternalId().UrlFormatString, pid);
+                        break;
+
+                    case Person:
+                        yield return string.Format(new ActorExternalId().UrlFormatString, pid);
+                        break;
+                }
+            }
+            
         }
     }
 }
