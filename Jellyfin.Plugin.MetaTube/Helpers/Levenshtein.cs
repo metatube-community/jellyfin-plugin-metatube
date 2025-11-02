73ced6eea9cb8d43d20c69b1b3d5ebd123a6531e
xjasonlyu <xjasonlyu@gmail.com>
2025-11-02 15:31:58 -0500

    clean comments

--- Jellyfin.Plugin.MetaTube/Helpers/Levenshtein.cs
+++ Jellyfin.Plugin.MetaTube/Helpers/Levenshtein.cs
@@ -1,16 +1,7 @@
 namespace Jellyfin.Plugin.MetaTube.Helpers;
 
-/// <summary>
-/// Measures the difference between two strings.
-/// Uses the Levenshtein string difference algorithm.
-/// </summary>
 public static class Levenshtein
 {
-    /// <summary>
-    /// Compares the two values to find the minimum Levenshtein distance. 
-    /// Thread safe.
-    /// </summary>
-    /// <returns>Difference. 0 complete match.</returns>
     public static int Distance(string value1, string value2)
     {
         if (value2.Length == 0)
@@ -20,7 +11,6 @@ public static class Levenshtein
 
         int[] costs = new int[value2.Length];
 
-        // Add indexing for insertion to first row
         for (int i = 0; i < costs.Length;)
         {
             costs[i] = ++i;
@@ -28,18 +18,15 @@ public static class Levenshtein
 
         for (int i = 0; i < value1.Length; i++)
         {
-            // cost of the first index
             int cost = i;
             int previousCost = i;
 
-            // cache value for inner loop to avoid index lookup and bonds checking, profiled this is quicker
             char value1Char = value1[i];
 
             for (int j = 0; j < value2.Length; j++)
             {
                 int currentCost = cost;
 
-                // assigning this here reduces the array reads we do, improvement of the old version
                 cost = costs[j];
 
                 if (value1Char != value2[j])
@@ -57,10 +44,6 @@ public static class Levenshtein
                     ++currentCost;
                 }
 
-                /*
-                 * Improvement on the older versions.
-                 * Swapping the variables here results in a performance improvement for modern intel CPU’s, but I have no idea why?
-                 */
                 costs[j] = currentCost;
                 previousCost = currentCost;
             }
